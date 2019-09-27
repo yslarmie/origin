62967bbf1af20d4451996102e097315855853bee
Slarmie <yassers_za@hotmail.com>
2019-09-27 11:01:28 +0200

    add public IP to ubuntu instance

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "example" {
 	ami	= "ami-07d0cf3af28718ef8"
 	instance_type = "t2.micro"
 	subnet_id	= "subnet-003e6aeb593c71a64"
+	associate_public_ip_address = true
 
 tags	= {
 	Name = "terraform-example-4"
